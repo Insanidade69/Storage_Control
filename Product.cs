4e95bbcd58a336c3b8a776bb1e2cc0627a337026
Marcelo <mvinicius.louza@gmail.com>
2022-01-10 17:11:00 -0300

    Adicionar arquivos de projeto.

---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Storage_Control
+{
+    internal class Product
+    {
+        public string Name;
+        public int Amount;
+        public double Price;
+
+        public double TotalValue()
+        {
+            return Price * Amount;
+        }
+        public override string ToString()
+        {
+            return "Product name: " + Name 
+                + ". Price: US$ " + Price.ToString("F2", CultureInfo.InvariantCulture) 
+                + ". Amount: " + Amount.ToString() 
+                + ". Total Value: US$ " + TotalValue().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Storage_Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+
+            Console.WriteLine("-=-=-=-=-=-=-= Storage Control =-=-=-=-=-=-=-");
+            Console.WriteLine("What infos of product?");
+            Console.Write("Name: ");
+            p.Name = Console.ReadLine();
+
+            Console.WriteLine("What price of product?");
+            Console.Write("Price: US$ ");
+            p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("What amount in storage of product?");
+            Console.Write("Amount: ");
+            p.Amount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+            Console.WriteLine("Product in storage: " + p);
+        }
+    }
+}
